2d151b954416f957ceb11fb5f7aadecf2ce85195
Korn Piboonsirikul <korn@buzzebees.com>
2023-04-17 16:58:37 +0700

    train hamburger menu animation

--- src/CustomNavbar.jsx
+++ src/CustomNavbar.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "./Navbar.css";
 import { useWindowDimensions, useCurrentPossition } from "./useWindowDimension";
 
 const navbar = ({ refOne, refTwo }) => {
-
-  const { height } = useWindowDimensions();
+  const [showMenu, setShowMenu] = useState(false);
+  const { height, width } = useWindowDimensions();
   const positionY = useCurrentPossition();
 
   const scrollToOne = () => {
@@ -13,36 +14,58 @@ const navbar = ({ refOne, refTwo }) => {
     refTwo.current.scrollIntoView();
   };
 
+  const toggleMenu = () => {
+    setShowMenu(prevValue => {
+      return !prevValue
+    })
+  };
+
   return (
-    <div className="nevbar">
-      <div className="leftside">
-        sticky navbar
+    <>
+      <div className="nevbar">
+        <div className="leftside">sticky navbar</div>
+        {width > 675 ? (
+          <ul className="rigthside">
+            <li
+              className={Math.ceil(positionY) == 0 ? "active" : ""}
+              onClick={scrollToOne}
+            >
+              {/* <a href="#section-1">section 1</a>  */}
+              section 1
+            </li>
+            <li
+              className={Math.ceil(positionY) == height ? "active" : ""}
+              onClick={scrollToTwo}
+            >
+              {/* <a href="#section-2">section 2</a>  */}
+              section 2
+            </li>
+          </ul>
+        ) : (
+          <div className={`hamburger${showMenu ? " active" : ""}`} onClick={toggleMenu}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
       </div>
-      <ul className="rigthside">
-        <li
-          className={
-            Math.ceil(positionY) == 0
-              ? "active"
-              : ""
-          }
-          onClick={scrollToOne}
-        >
-          {/* <a href="#section-1">section 1</a>  */}
-          section 1
-        </li>
-        <li
-          className={
-            Math.ceil(positionY) == height
-              ? "active"
-              : ""
-          }
-          onClick={scrollToTwo}
-        >
-          {/* <a href="#section-2">section 2</a>  */}
-          section 2
-        </li>
-      </ul>
-    </div>
+        <ul className={`hamburger-menu${showMenu ? " show" : ""}`}>
+          <li
+            className={Math.ceil(positionY) == 0 ? "active" : ""}
+            onClick={scrollToOne}
+          >
+            {/* <a href="#section-1">section 1</a>  */}
+            section 1
+          </li>
+          <li
+            className={Math.ceil(positionY) == height ? "active" : ""}
+            onClick={scrollToTwo}
+          >
+            {/* <a href="#section-2">section 2</a>  */}
+            section 2
+          </li>
+        </ul>
+    </>
   );
 };
 
